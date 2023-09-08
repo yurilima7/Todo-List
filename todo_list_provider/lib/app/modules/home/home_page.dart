2f55bf36018b2f9c54f177375a280a059d77bc09
Yuri Lima <61335084+yurilima7@users.noreply.github.com>
2023-09-08 11:16:43 -0300

    AppBar, Filters and Header

--- todo_list_provider/lib/app/modules/home/home_page.dart
+++ todo_list_provider/lib/app/modules/home/home_page.dart
@@ -1,17 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_icons.dart';
 import 'package:todo_list_provider/app/modules/home/widgets/home_drawer.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_filters.dart';
+import 'package:todo_list_provider/app/modules/home/widgets/home_header.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  const HomePage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Home Page'),),
-        drawer: HomeDrawer(),
-        body: Container(),
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: context.primaryColor,
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(TodoListIcons.filter),
+            itemBuilder: (_) => [
+              const PopupMenuItem<bool>(
+                child: Text('Mostrar tarefas concluídas'),
+              )
+            ],
+          )
+        ],
+      ),
+
+      backgroundColor: const Color(0xFFFAFBFE),
+      drawer: HomeDrawer(),
+
+      body: LayoutBuilder(
+        builder: (context, constraints) => SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
+              maxWidth: constraints.maxWidth,
+            ),
+
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: IntrinsicHeight(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const HomeHeader(),
+                    const HomeFilters(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
